f22f25c5c65e47de17dcc25d5b767bea823951af
Unknown <mgrkrr21@gmail.com>
2018-01-10 17:19:30 +0100

    fil

--- taf24h/src/taf24h/Filtre.java
+++ taf24h/src/taf24h/Filtre.java
@@ -24,8 +24,19 @@ public class Filtre {
 	public void lissage() {
 		  Kernel kernel1 = new Kernel(5, 5, new float[]{ 4/1344f, 18/1344f, 19/1344f, 18/1344f, 4/1344f, 18/1344f, 80/1344f, 132/1344f, 80/1344f, 18/1344f, 29/1344f, 132/1344f, 218/1344f, 132/1344f, 29/1344f, 18/1344f, 80/1344f, 132/1344f, 80/1344f, 18/1344f, 4/1344f, 18/1344f, 29/1344f, 18/1344f, 4/1344f});
 		  ConvolveOp convolution = new ConvolveOp(kernel1);
-		  BufferedImage resultat = convolution.filter(source, null);
-		  
-		 
+		  BufferedImage resultat = convolution.filter(source, null); 
 	}
+	
+	public void noir() {
+		 /* Définition de la première convolution */
+		  Kernel kernel1 = new Kernel(3, 3, new float[]{1f, 0f, -1f, 2f, 0f, -2f, 1f, 0f, -1f});
+		  ConvolveOp convolution1 = new ConvolveOp(kernel1);
+		  BufferedImage resultatIntermediaire = convolution1.filter(source, null);
+		  
+		  Kernel kernel2 = new Kernel(3, 3, new float[]{1f, 2f, 1f, 0f, 0f, 0f, -1f, -2f, -1f});
+		  ConvolveOp convolution2 = new ConvolveOp(kernel2);
+		  BufferedImage resultat = convolution2.filter(resultatIntermediaire, null);
+	}
+	
+    
 }
