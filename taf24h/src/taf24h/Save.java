16423adc69667b723247c5d6c22ec4d774d4907c
Unknown <mgrkrr21@gmail.com>
2018-01-10 17:08:05 +0100

    fxml

--- taf24h/src/taf24h/Save.java
+++ taf24h/src/taf24h/Save.java
@@ -8,10 +8,15 @@ import javax.imageio.ImageIO;
 
 public class Save {
 
-	public Save(BufferedImage image, String imageName, String sourcepath) throws IOException{
+	public void Sauvegarder(BufferedImage image, String imageName, String sourcepath){
 		
 		File fileName = new File(sourcepath + imageName +".jpg"); //pour l'enregistrer dans un autre extensions il suffit de remplacer l'extension dans le code
-		ImageIO.write(image, "jpg", fileName);
+		try {
+			ImageIO.write(image, "jpg", fileName);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
