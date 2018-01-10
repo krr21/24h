5a5e414a869da381655473dda286f85c970c27d3
e165986 <e165986@E-0383.univ-lemans.fr>
2018-01-10 16:45:45 +0100

    save

--- taf24h/src/taf24h/Save.java
+++ taf24h/src/taf24h/Save.java
@@ -1,9 +1,17 @@
 package taf24h;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Save {
 
-	public Save() {
-		// TODO Auto-generated constructor stub
+	public Save(BufferedImage image, String imageName, String sourcepath) throws IOException{
+		
+		File fileName = new File(sourcepath + imageName +".jpg"); //pour l'enregistrer dans un autre extensions il suffit de remplacer l'extension dans le code
+		ImageIO.write(image, "jpg", fileName);
 	}
 
 }
